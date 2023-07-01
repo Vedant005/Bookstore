796ebf4eaa41e91bd92040922838f352a2110876
Vedant005 <vedantkanekar909@gmail.com>
2023-07-02 01:45:39 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,48 @@ import "./header.css"
 import { NavLink } from "react-router-dom";
 
 const getStyle=({isActive})=>({
-    color: isActive ? "red" : ""
+    color: isActive ? "red" : "",
+    border: isActive ? "1 rem" : "0.5rem",
+    padding: isActive ? "3 rem" : "2rem",
+    margin: "1rem",
+ textDecoration : "none"
 })
 
 export function Header() {
     return (
         <header>
-              <h1 class="title">  MAGNET STORE</h1>
+          <div class="container">
+              <span class="title">  magnet store</span>
             <nav>
         <NavLink style={getStyle} to="/">
           Home
         </NavLink>
         
-        <NavLink style={getStyle} to="/category">
-          Category
+        <NavLink style={getStyle} to="/products">
+          Products
         </NavLink>
         </nav>
     
-      <label >
+      <label class="search-bar">
         <input type="text"/>
       </label>
+     <nav>
+     <NavLink style={getStyle} to="/profile">
+          Profile   </NavLink>
+        <NavLink style={getStyle} to="/wishlist">
+          Wishlist
+        </NavLink>
+        <NavLink style={getStyle} to="/cart">
+          Cart
+        </NavLink>
+     </nav>
+     
+     
+     
+     
+      </div>
       </header>
     );
   }
+
   
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Header } from "../components/Header.jsx";
-
+import { Footer } from "../components/Footer.jsx";
+import  bowl from "../images/bowl.jpg";
+import  glass from "../images/glass.jpg";
+import  plates from "../images/plates.jpg";
+import  dinnerset from "../images/dinnerset.jpg";
+import  milton from "../images/milton.png";
+import  Borosil from "../images/Borosil.png";
+import  treo from "../images/treo.png";
+import  claycraft from "../images/claycraft.jpg";
 import "./Home.css"
 
 export default function Home() {
   return (
-   <div>
+   <div class="main-container">
  <Header />
  
    
@@ -17,6 +25,52 @@ export default function Home() {
     </div>
     
     </div>
+
+    <div class="category-container">
+          <h1>Featured Categories</h1>
+
+          <div>
+            <img class="category-image" src={bowl}alt="bowl-image"/>
+           <div>Bowl</div>
+          </div>
+
+        
+          <div>
+            <img class="category-image" src={glass}alt="glass-image"/>
+            <div>Glass</div>
+          </div>
+
+          <div>
+            <img class="category-image" src={plates}alt="plates-image"/>
+            <div>Plates</div>
+          </div>
+          <div>
+            <img class="category-image" src={dinnerset}alt="dinnerset-image"/>
+            <div>Dinnerset</div>
+          </div>
+    </div>
+    <h1> Featured Brands</h1>
+    <div class="featured-brands">
+       
+      < div>
+            <img class="brand-image" src={milton}alt="milton-image"/>
+          
+          </div>
+          < div>
+            <img class="brand-image" src={Borosil}alt="borosil-image"/>
+          
+          </div>
+          < div>
+            <img class="brand-image" src={treo}alt="treo-image"/>
+          
+          </div>
+          < div>
+            <img class="brand-image" src={claycraft}alt="claycraft-image"/>
+          
+          </div>
+          
+    </div>
+    <Footer/>
     </div>
   );
 }
